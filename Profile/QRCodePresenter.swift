70b89c7534e3266feca8f9be60c77cb67be78240
KaitoMuraoka <kaitoboshi@icloud.com>
2025-09-18 00:55:10 +0900

    Presenterを追加

---
+++ Profile/QRCodePresenter.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import Combine
+import QRCode
+import CoreGraphics
+
+final class QRCodePresenter: ObservableObject {
+    @Published var qrImage: CGImage?
+    @Published var qrCodeUrlString = "https://x.com/Ktombow1110"
+    @Published var qrCodeImageUrlString = "https://avatars.githubusercontent.com/u/70003919?v=4"
+}
+
+extension QRCodePresenter {
+    func viewDidLoad() {
+        Task {
+            if let url = URL(string: qrCodeImageUrlString) {
+                let logo = await fetchCGImage(from: url)
+                qrImage = setupQRCode(for: qrCodeUrlString, logoImage: logo)
+            }
+        }
+    }
+    
+    
+    /// QRコード生成
+    private func setupQRCode(for text: String, logoImage: CGImage? = nil) -> CGImage? {
+        guard let doc = try? QRCode.Document(utf8String: text) else { return nil }
+        doc.design.style.backgroundFractionalCornerRadius = 3.0
+        
+        if let logoImage {
+            doc.logoTemplate = QRCode.LogoTemplate.CircleCenter(image: logoImage)
+        }
+        
+        return try? doc.cgImage(dimension: 400)
+    }
+    
+    /// URLからCGImageを非同期取得
+    private func fetchCGImage(from url: URL) async -> CGImage? {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let uiImage = UIImage(data: data) {
+                return uiImage.cgImage
+            }
+        } catch {
+            print("画像取得失敗: \(error)")
+        }
+        return nil
+    }
+}
---
+++ Profile/QRCodeView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+import Combine
+
+struct QRCodeView: View {
+    @ObservedObject var presenter: QRCodePresenter
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            VStack {
+                qrCode
+            }
+            .padding(.vertical, 1.618*50)
+            .padding(.horizontal, 50)
+            .background(.white)
+            .cornerRadius(8)
+            .clipped()
+            .shadow(color: .gray.opacity(0.7), radius: 5)
+        }
+        .padding()
+        .onAppear { presenter.viewDidLoad() }
+    }
+    
+    @ViewBuilder
+    private var qrCode: some View {
+        if let cg = presenter.qrImage {
+            Image(decorative: cg, scale: 1.0, orientation: .up)
+                .interpolation(.none)
+                .antialiased(false)
+                .resizable()
+                .frame(width: 200, height: 200)
+        } else {
+            Text("QR を生成中...")
+                .foregroundStyle(.secondary)
+        }
+    }
+}
+
+#Preview {
+    QRCodeView(presenter: .init())
+}
